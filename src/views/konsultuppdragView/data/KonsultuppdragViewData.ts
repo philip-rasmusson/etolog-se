e9899baafa02aaefc3831b4cb3eb797a9af4d4f5
Philip Rasmusson <studiobigmouth@gmail.com>
2021-05-06 10:05:08 +0200

    minor changes, updated data-etologer

--- src/views/konsultuppdragView/data/KonsultuppdragViewData.ts
+++ src/views/konsultuppdragView/data/KonsultuppdragViewData.ts
@@ -1,7 +1,7 @@
 const  backgroundColor = { backgroundColor: '#c5a086' }
 const  pageTitle = 'Konsultuppdrag'
 const  pageSubheading = 'Som djurhållare ansvarar du för djurens välfärd och det krävs att du har tillräcklig kunskap om de djur som du ombesörjer'
-const  pageParagraph1 = 'Våra konsulter kan hjälpa din verksamhet att utvecklas och blomstra. Etologers yrkesprofession är otroligt bred och idag ser vi erologer i verksamheter, som veterinärskliniker, zoobutiker, lantbruk, branschorganisationer, försökdjursenheter, universitet, beöksgårdar, myndigheter med mera.'
+const  pageParagraph1 = 'Våra konsulter kan hjälpa din verksamhet att utvecklas och blomstra. Etologers yrkesprofession är otroligt bred och idag ser vi erologer i verksamheter, som veterinärskliniker, zoobutiker, lantbruk, branschorganisationer, försökdjursenheter, universitet, beöksgårdar, myndigheter med mera. Kontakta oss på info@etolog.se'
 //Section Two
 const  imgPlacement = 'left'
 const  sectionTwoParagraph1 = 'Lyft din verksamhet med en etolog!'
