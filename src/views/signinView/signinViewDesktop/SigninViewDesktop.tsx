0bae2b11c6d8b7cb4a835daf519cbb145f8ee9b6
Philip Rasmusson <studiobigmouth@gmail.com>
2021-05-19 11:31:33 +0200

    minor updates

--- src/views/signinView/signinViewDesktop/SigninViewDesktop.tsx
+++ src/views/signinView/signinViewDesktop/SigninViewDesktop.tsx
@@ -1,9 +1,21 @@
 import './SigninViewDesktop.css'
+import SigninHeader from '../img/sigingHeader.jpg'
+import { HeaderDesktop } from '../../../components/header/headerDesktop/HeaderDesktop'
 
 export const SigninViewDesktop = () => {
   return (
-    <div className="signin-desktop-wrapper">
-      <h1>This is signin</h1>
-    </div>
+    <>
+      <HeaderDesktop headerImg={SigninHeader} />
+      <div className="signin-desktop-wrapper">
+        <div className="signin-desktop-form-wrapper">
+
+          <form className="signin-desktop-form">
+            <label>Username: </label><input type="text" /><br />
+            <label>Password: </label><input type="text" /><br />
+            <input type="submit" value="Log in" />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
